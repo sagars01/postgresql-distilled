957d92eea57ab628542ac915fa4d6b7406d21798
Sagar <sagarmoysengupta@gmail.com>
2023-08-16 10:41:36 +0530

    to update query and add mock-data

--- Chapter 01/create-tables.sql
+++ Chapter 01/create-tables.sql
@@ -1,41 +1,14 @@
-
-
--- Create the "employees" table to store employee information
 CREATE TABLE employees (
-    employee_id SERIAL PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    phone_number VARCHAR(20),
-    hire_date DATE NOT NULL,
-    job_title VARCHAR(100) NOT NULL,
-    department_id INT REFERENCES departments(department_id) ON DELETE SET NULL,
-    manager_id INT REFERENCES employees(employee_id) ON DELETE SET NULL
-);
-
-CREATE TABLE salaries (
-    salary_id SERIAL PRIMARY KEY,
-    employee_id INT REFERENCES employees(employee_id) ON DELETE CASCADE,
-    salary_amount NUMERIC(10, 2) NOT NULL,
-    start_date DATE NOT NULL,
-    end_date DATE
+	employee_id VARCHAR(21) PRIMARY KEY,
+    first_name VARCHAR(50),
+    gender VARCHAR(10),
+    start_date DATE,
+    last_login_time TIME,
+    salary INTEGER,
+    bonus_percent DECIMAL(5,3),
+    senior_management BOOLEAN,
+    team VARCHAR(50)
 );
 
 
--- Create the "attendance" table to store employee attendance records
-CREATE TABLE attendance (
-    attendance_id SERIAL PRIMARY KEY,
-    employee_id INT REFERENCES employees(employee_id) ON DELETE CASCADE,
-    check_in TIMESTAMP NOT NULL,
-    check_out TIMESTAMP
-);
-
-
--- Create the "tasks" table to store employee tasks
-CREATE TABLE tasks (
-    task_id SERIAL PRIMARY KEY,
-    employee_id INT REFERENCES employees(employee_id) ON DELETE CASCADE,
-    task_description TEXT NOT NULL,
-    due_date DATE NOT NULL,
-    status VARCHAR(20) NOT NULL
-);
+SELECT * FROM employees
